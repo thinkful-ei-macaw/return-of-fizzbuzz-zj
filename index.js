56b45e50aa06f8e7f2227326dbf179ee50409a27
Zac Barreca <zacbarreca@gmail.com>
2020-02-12 14:38:41 -0500

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // your code here
+$(function(){
+    $('number-chooser').submit( event => {
+        event.preventDefault();
+
+        $(".js-results").empty();
+
+        const num = parseInt( $(event.currentTarget).find('#number-choice').val());
+
+        const results = [];
+
+        for (let i = 0; i <= num; i++) {
+            if (i % 15 === 0) {
+                results.push($('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>'))
+            } else if (i % 5 === 0) { 
+                results.push($('<div class="fizz-buzz-item buzz"><span>buzz</span></div>'))
+            } else if (i % 3 === 0) {
+                results.push($('<div class="fizz-buzz-item fizz"><span>fizz</span></div>'))
+            } else {
+                results.push($(`<div class="fizz-buzz-item"><span>${i}</span></div>`))
+            }
+        }
+        $('.js-results').append(results);
+    });
+});
